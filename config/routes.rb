77484d5579c96ebe9e85c74f5cb63e3febc2ee13
Alex Tsiras <tsirasalexis@yahoo.gr>
2017-06-23 13:44:17 +0300

    add usersController and user#show user#edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#show'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   resources :posts, only: [:new, :create]
 end
